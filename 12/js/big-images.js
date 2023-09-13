057a3c80cf1cd823f72caf3dd0d08e2a8bdeb5d7
Keks <keks@htmlacademy.ru>
2023-09-13 17:27:42 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/big-images.js
@@ -0,0 +1,93 @@
+//Количество комментариев под фото.
+const COMMENTS_PER_PAGE = 5;
+
+const pictureElement = document.querySelector('.big-picture');
+const commentsCount = document.querySelector('.comments-count');
+const pictureCloseButton = pictureElement.querySelector('.big-picture__cancel');
+const commentCountElement = document.querySelector('.social__comment-count');
+const body = document.querySelector('body');
+const socialCommentsLoader = pictureElement.querySelector('.comments-loader');
+const socialCommentsList = pictureElement.querySelector('.social__comments');
+const commentElement = pictureElement.querySelector('.big-picture__social').querySelector('.social__comment');
+
+let commentsShown = 0;
+let comments = [];
+//Создание одного комментария
+const createComment = ({avatar, message, name}) => {
+  const comment = commentElement.cloneNode(true);
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+//Создание рандомных комментариев в модальном окне, под большим фото и отображение 5 комментариев на странице.
+const renderComments = () => {
+  commentsShown += COMMENTS_PER_PAGE;
+  if (commentsShown >= comments.length) {
+    socialCommentsLoader.classList.add('hidden');
+    commentsShown = comments.length;
+  } else {
+    socialCommentsLoader.classList.remove('hidden');
+  }
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < commentsShown; i++) {
+    const comment = createComment(comments[i]);
+    fragment.append(comment);
+  }
+
+  socialCommentsList.innerHTML = '';
+  socialCommentsList.append(fragment);
+  //Показываем сколько комментариев отображено из общего количества, и кол-ва COMMENTS_PER_PAGE.
+  commentCountElement.innerHTML = `${commentsShown} из <span class="comments-count">${comments.length}</span> комментариев`;
+};
+//Загрузка новой порции комментариев
+const onShowMoreComments = () => renderComments(comments);
+
+//Скрытие модалки
+const hideBigImage = () => {
+  pictureElement.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  //Обнуляем количество комметариев
+  commentsShown = 0;
+};
+
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    hideBigImage();
+  }
+}
+
+const onPictureCloseButton = () => {
+  hideBigImage();
+};
+//Поиск данных для лайков, описания
+const renderImageDetails = ({url, likes, description}) => {
+  pictureElement.querySelector('.big-picture__img img').src = url;
+  pictureElement.querySelector('.big-picture__img img').alt = description;
+  pictureElement.querySelector('.likes-count').textContent = likes;
+  pictureElement.querySelector('.social__caption').textContent = description;
+};
+//Открытие модалки
+const openBigImage = (data) => {
+  pictureElement.classList.remove('hidden');
+  body.classList.add('modal-open');
+  socialCommentsLoader.classList.add('hidden');
+  commentsCount.classList.add('hidden');
+  document.addEventListener('keydown', onDocumentKeydown);
+  renderImageDetails(data);
+  comments = data.comments;
+  if (comments.length > 0) {
+    renderComments(comments);
+  }
+};
+//отработчик для закрытия модалки по клику на крестик.
+pictureCloseButton.addEventListener('click', onPictureCloseButton);
+//отработчик для кнопки "Загрузить еще"
+socialCommentsLoader.addEventListener('click', onShowMoreComments);
+
+export { openBigImage, hideBigImage };
---
+++ 12/js/util.js
@@ -0,0 +1,42 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const ALERT_TIMEOUT = 5000;
+
+const errorAlert = (message) => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.zIndex = '100';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.textAlign = 'center';
+  messageAlert.style.fontSize = '26px';
+  messageAlert.style.backgroundColor = 'red';
+
+  messageAlert.textContent = message;
+
+  document.body.append(messageAlert);
+  setTimeout(() => {
+    messageAlert.remove();
+  }, ALERT_TIMEOUT);
+};
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export { getRandomArrayElement, getRandomInteger, isEscapeKey, errorAlert, debounce };
